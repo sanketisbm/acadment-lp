500358e4fb2d8d4ff2ab5495caf45cfe78a97d67
Dainik Patil <dainik.patil@isbm.org.in>
2025-11-29 14:23:59 +0530

    Title jain dainik

--- jain-landing-page/index.php
+++ jain-landing-page/index.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>ISBM University Chhattisgarh | Top University in India</title>
-
+    <title>Master of Business Administration (MBA)</title>
     <link href="assets/images/favicon/ju-favicon.ico" rel="icon" type="image/png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
